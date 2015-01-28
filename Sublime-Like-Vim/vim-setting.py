098cb05431dafaee923db6d077bc9fa64abaa5c6
ysocks <ysocks128@gmail.com>
2015-01-28 14:33:18 +0900

    Python script for install added.

---
+++ Sublime-Like-Vim/vim-setting.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+from os.path import expanduser
+
+if not os.path.isdir('./Sublime-Like-Vim'):
+    os.system('mkdir ./Sublime-Like-Vim')
+
+if not os.path.isdir('~/.vim'):
+    os.system('mkdir ~/.vim')
+
+if not os.path.isdir('~/.vim/colors'):
+    os.system('mkdir ~/.vim/colors')
+
+if not os.path.isdir('~/.vim/syntax'):
+    os.system('mkdir ~/.vim/syntax')
+
+os.system('wget -P ./Sublime-Like-Vim https://github.com/ysocks/Sublime-Like-Vim/archive/master.zip')
+os.system('unzip ./Sublime-Like-Vim/master.zip -d ./Sublime-Like-Vim')
+os.system('cp ./Sublime-Like-Vim/Sublime-Like-Vim-master/c.vim ~/.vim/syntax/c.vim')
+os.system('cp ./Sublime-Like-Vim/Sublime-Like-Vim-master/Sublime-Like-Vim.vim ~/.vim/colors/Sublime-Like-Vim.vim')
+os.system('rm -r -f ./Sublime-Like-Vim')
+
+opt = []
+opt.append('set ts=4')
+opt.append('set sw=4')
+opt.append('set mouse=a')
+opt.append('set expandtab')
+opt.append('set nu')
+opt.append('set ai')
+opt.append('syntax enable')
+opt.append('colorscheme Sublime-Like-Vim')
+opt.append('set backspace=indent,eol,start')
+
+if not os.path.isfile('~/.vimrc'):
+    os.system('>~/.vimrc')
+
+home = expanduser("~")
+
+with open(home+'/.vimrc','r') as f:
+    vimrc = f.read()
+
+for option in opt:
+    if (vimrc.find(option) == -1):
+        os.system('echo "'+option+'" >> ~/.vimrc')
+
